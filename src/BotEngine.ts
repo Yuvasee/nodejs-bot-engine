d31d03996396a4110f1c62a0bc9db30f68e061f2
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2020-01-13 00:15:04 +0200

    Engine reimplemented

--- src/BotEngine.ts
+++ src/BotEngine.ts
@@ -1,44 +1,66 @@
 import TelegramBot = require('node-telegram-bot-api');
-import Command from './interfaces/Command';
-import CommandSet from './interfaces/CommandSet';
+import { BotConfig, BotApi, Message, Sheet, Command, Feature, Channel } from './interfaces';
+import * as re from './re';
 
-export default class BotEngine {
+export interface IBotEngine {
+    registerCommand: (command: Command) => void;
+    registerFeature: (feature: Feature) => void;
+    registerFallback: () => void;
+}
+
+export default class BotEngine implements IBotEngine {
     private registeredCommands = new Set<string>([]);
+    private api: TelegramBot;
+    private botApi: BotApi;
 
-    constructor(private api: TelegramBot) {}
+    constructor(config: BotConfig) {
+        this.api = new TelegramBot(config.telegramBotToken, { polling: true });
+    }
 
-    public registerCommand(this: BotEngine, command: Command) {
-        if (this.validateCommand(command)) {
-            this.api.onText(command.trigger, command.reaction(this.api));
-            this.registeredCommands.add(command.name);
-
-            console.log('Registered command: ' + command.name);
+    public registerCommand(command: Command) {
+        const validationResult = this.validateCommand(command);
+        if (validationResult) {
+            throw new Error(validationResult);
         }
+
+        const { triggers, reaction, name } = command;
+
+        triggers.forEach(trigger =>
+            this.api.onText(trigger, (msg: TelegramBot.Message) => {
+                const botApi = this.createApiTelegram(msg);
+                const message: Message = {
+                    channel: Channel.Telegram,
+                    text: msg.text,
+                    userId: msg.from.id.toString(),
+                };
+                reaction(botApi, message);
+            }),
+        );
+
+        this.registeredCommands.add(name);
+        console.log('Registered command: ' + name);
 
         return this;
     }
 
-    public registerCommandSet(this: BotEngine, commandSet: CommandSet) {
-        const { name: setName, commands, fallback } = commandSet;
+    public registerFeature(feature: Feature) {
+        const validationResult = this.validateFeature(feature);
+        if (validationResult) {
+            throw new Error(validationResult);
+        }
+
+        const { name, commands, fallback } = feature;
         const commandNames = Object.keys(commands);
 
-        if (this.registeredCommands.has(setName)) {
-            throw new Error(`Command names collision occured: ${setName}. Commands should have unique names.`);
-        }
-        this.registeredCommands.add(setName);
-
-        console.log(`Command set: ${setName} ---`);
+        this.registeredCommands.add(name);
+        console.log(`Command set: ${name} ---`);
 
         commandNames.forEach(commandName => {
-            // matches "setName commandName..."
-            const reKnownCmd = `^${setName}[ \\t]+${commandName}([ \\t]+.*)?$`;
-            commands[commandName].trigger = new RegExp(reKnownCmd);
+            commands[commandName].triggers.push(re.featureCommand(name, commandName));
             this.registerCommand(commands[commandName]);
         });
 
-        // matches "setName" and "setName not(one|of|commandNames)..."
-        const reFallbackCmd = `^${setName}(?!\\S|([ \\t]+)?(${commandNames.join('|')}))`;
-        fallback.trigger = new RegExp(reFallbackCmd);
+        fallback.triggers.push(re.featureFallback(name, commandNames));
         this.registerCommand(fallback);
 
         console.log(`---`);
@@ -46,39 +68,53 @@ export default class BotEngine {
         return this;
     }
 
-    public registerFallback(this: BotEngine) {
-        // matches minus command
-        const reMinusSum = '-\\d+(?:[.,]\\d+)?(?:[+\\-*\\/]\\d+(?:[.,]\\d+)?)*';
-        const commands = [...this.registeredCommands];
-        const commandsJoined = commands.join('|');
+    public registerFallback() {
+        const commandNames = [...this.registeredCommands];
 
-        // matches all but valid commands
-        const reFallbackGlobal = `^(?!(${reMinusSum}|${commandsJoined})).+|^(${commandsJoined})\\w+.*`;
+        const triggers = [re.globalFallback(commandNames)];
+        const reaction = (bot: BotApi) => {
+            bot.sendMessage(`commands:\n${commandNames.join('\n')}`);
+        };
 
-        this.registerCommand({
-            name: 'global fallback',
-
-            trigger: new RegExp(reFallbackGlobal),
-
-            reaction: botApi => (msg, match) => {
-                botApi.sendMessage(msg.chat.id, `commands: \n${commands.join('\n')}`);
-            },
-        });
-
-        return this;
+        this.registerCommand({ name: 'global fallback', triggers, reaction });
     }
 
-    private validateCommand(command: Command): boolean {
+    private validateCommand(command: Command): string | undefined {
         if (this.registeredCommands.has(command.name)) {
-            console.error(`Command names collision occured: ${command.name}. Commands should have unique names.`);
-            return false;
+            return `Command names collision occured: ${command.name}. Commands should have unique names.`;
         }
 
-        if (!command.trigger) {
-            console.error(`Command ${command.name} has no trigger`);
-            return false;
+        if (!command.triggers?.length) {
+            return `Command ${command.name} has no triggers`;
         }
 
-        return true;
+        if (command.triggers.map(t => t instanceof RegExp).includes(false)) {
+            return `Command ${command.name} triggers array contains invalid element`;
+        }
+    }
+
+    private validateFeature(feature: Feature): string | undefined {
+        if (this.registeredCommands.has(feature.name)) {
+            return `Command names collision occured: ${name}. Commands should have unique names.`;
+        }
+
+        if (!feature.commands?.length) {
+            return `Feature ${feature.name} has no commands`;
+        }
+    }
+
+    // TODO: extract to BotApi class with constructor overloading
+    private createApiTelegram(msg: TelegramBot.Message): BotApi {
+        return {
+            sendMessage: (text: string) => {
+                this.api.sendMessage(msg.chat.id, text);
+            },
+            sendSheet: (sheet: Sheet) => {
+                // this.api.sendMessage(msg.chat.id, sheetToMsg(sheet));
+            },
+            sendFile: (file: Buffer) => {
+                // this.api.sendDocument(msg.chat.id, doc);
+            },
+        };
     }
 }
